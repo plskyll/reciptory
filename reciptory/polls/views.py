2f3c8619947f124fa4be9d6edc29f0476bd9c0b1
plskyll <yuliia.v.plysiuk@ukd.edu.ua>
2025-04-15 22:21:43 +0300

    10.1

--- reciptory/polls/views.py
+++ reciptory/polls/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistrationForm
 
 # Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('polls:question_list')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'register.html', {"form": form})
